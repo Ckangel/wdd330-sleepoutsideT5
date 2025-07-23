3b5842f3f43c8d659a4612cd01c6bfae08da8aef
joojobill <idhamzzyisaac@gmail.com>
2025-07-23 13:53:35 +0000

    IH--individual3

--- src/js/product.js
+++ src/js/product.js
@@ -1,13 +1,12 @@
 
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
- 
-loadHeaderFooter();
- 
-const productId = getParam("product");
-const dataSource = new ProductData("tents");
 
-const product = new ProductDetails(productId, dataSource);
+loadHeaderFooter();
+
+const dataSource = new ProductData("tents");
+const productID = getParam("product");
+
+const product = new ProductDetails(productID, dataSource);
 product.init();
- 
- 
