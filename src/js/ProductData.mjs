663b7d6f04d56470fe208c89799b9612764bf5cc
AdongoNat <danieladongonat@gmail.com>
2025-07-23 10:54:22 +0000

    week 3 main

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -20,4 +20,12 @@ export default class ProductData {
     const products = await this.getData();
     return products.find((item) => item.Id === id);
   }
+   async search(query) {
+    const allProducts = await this.getData(); // getData should return all products if no category is passed
+    return allProducts.filter(product =>
+      product.Name.toLowerCase().includes(query.toLowerCase()) ||
+      product.Brand.toLowerCase().includes(query.toLowerCase())
+    );
+  }
 }
+
