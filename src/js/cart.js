b332a14f93210262bd4ed4d71aebc812b02279c7
adetokefad <adetokefaderin@gmail.com>
2025-07-23 16:37:30 +0100

    REMOVE FROM CART

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,8 @@
-import { getLocalStorage, setLocalStorage, loadHeaderFooter } from "./utils.mjs";
+import {
+  getLocalStorage,
+  setLocalStorage,
+  loadHeaderFooter,
+} from "./utils.mjs";
 
 loadHeaderFooter();
 
@@ -6,7 +10,8 @@ function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || [];
 
   if (cartItems.length === 0) {
-    document.querySelector(".product-list").innerHTML = "<p>Your cart is empty.</p>";
+    document.querySelector(".product-list").innerHTML =
+      "<p>Your cart is empty.</p>";
     document.getElementById("cart-footer-container").innerHTML = "";
     return;
   }
@@ -55,7 +60,7 @@ function displayCartTotal() {
   if (cartItems.length > 0) {
     const total = cartItems.reduce(
       (sum, item) => sum + Number(item.FinalPrice),
-      0
+      0,
     );
 
     const template = document.getElementById("cart-footer-template");
