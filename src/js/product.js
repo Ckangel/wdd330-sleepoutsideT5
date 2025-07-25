78963a8ea28ce45243fd678d163658b00575d38b
AdongoNat <danieladongonat@gmail.com>
2025-07-25 12:11:33 +0000

    typo

--- src/js/product.js
+++ src/js/product.js
@@ -7,7 +7,7 @@ import { getParam, loadHeaderFooter, updateCartCount } from "./utils.mjs";
 import ProductData from "./ProductData.mjs"; 
 // ProductData handles fetching product data from the API
 
-import ProductDetails from "./ProductDeatils.mjs"; 
+import ProductDetails from "./ProductDetails.mjs"; 
 // ProductDetails is the class responsible for rendering a single product’s details
 
 // Load header and footer on the page
